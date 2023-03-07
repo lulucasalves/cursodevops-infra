e4de536ccf3454ee2ff910eb8d84b2ba8e18ab5b
lucas <lucas.alves.supus@outlook.com>
2023-03-07 14:39:00 -0300

    feat: add docker swarm

--- docker/provision.sh
+++ docker/provision.sh
@@ -5,3 +5,4 @@ sudo yum install docker-ce docker-ce-cli containerd.io -y
 sudo systemctl start docker
 sudo systemctl enable docker
 cp -r /vagrant/docker-compile/ /root
+cp -r /vagrant/docker-compile-stage/ /root
